e0babddd1deb3ed93c95e796da22ca00d1dadd44
Andrew Chap <31779927+AndrewChap@users.noreply.github.com>
2019-01-23 19:37:10 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='SymPint',
+      version='0.1',
+      description='An algebraic manipulation and dimensional analysis tool using SymPy and Pint',
+      url='http://github.com/AndrewChap/sympint',
+      author='Andrew Chap',
+      author_email='andrew@andrewchap.com',
+      license='MIT',
+      packages=['sympint'])
