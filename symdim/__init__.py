5be1d8ffa5cc74c5e5b3fc0ac9fc7de5b10c50b8
Andrew Chap <31779927+AndrewChap@users.noreply.github.com>
2019-06-12 13:43:30 -0600

    Changing all instances of "SymPint" to "SymDim", ignoring complex outputs in SymDim.solve_for()
    Changing all instances of "SymPint" to "SymDim" to reflect name change.
    Ignoring complex outputs in SymDim.solve_for().  This package isn't yet intended for complex values (and probably never will be...) and when sympy.solve was returning complex values it was causing an error in SymDim.evaluate()

--- symdim/__init__.py
+++ symdim/__init__.py
@@ -1,13 +1,13 @@
 import sympy
 from astropy import units as u
 
-# Class that inherits from sympy.Symbol but also gets the 'parent' attribute so we can find the Sympint instance from the symbol
+# Class that inherits from sympy.Symbol but also gets the 'parent' attribute so we can find the SymDim instance from the symbol
 class Symbol(sympy.Symbol):
     def __init__(self,name,nonnegative=True,parent=None):
         super().__new__(name=name,nonnegative=nonnegative,real=True,cls=sympy.Symbol) # call sympy.symbol __new__ method
         self.parent = parent # assign parent
         
-class SymPint:
+class SymDim:
     def __init__(self,
                  name=None,
                  unit=None,
@@ -20,11 +20,11 @@ class SymPint:
         
         
         if isinstance(name,str):
-            # e.g. SymPint('a')
+            # e.g. SymDim('a')
             self.name = name
             self.symbol = Symbol(self.name,nonnegative=nonnegative,parent=self)
         else:
-            # e.g. SymPint(4) or SymPint(expression=a*b)
+            # e.g. SymDim(4) or SymDim(expression=a*b)
             self.name = '\mathrm{unnamed\,expr.}'
             try:
                 self.symbol = sympy.Number(name)
@@ -83,6 +83,7 @@ class SymPint:
             
     # sets everything equal to other except for name
     def equals(self,other):
+        #pdb.set_trace()
         if self.unit is None:
             self.unit = other.unit
         if other.unit is not None and self.unit.decompose() != other.unit.decompose():
@@ -91,7 +92,7 @@ class SymPint:
             print('Warning: units of {} and {} do not match'.format(self,other))
             self.unit = other.unit
         other_expression = other.get_expression()
-        while isinstance(other_expression,SymPint):
+        while isinstance(other_expression,SymDim):
             other_expression = other_expression.expression
         self.set_expression(other_expression)
         
@@ -105,15 +106,16 @@ class SymPint:
         # for example, if we want to solve z=x*y for y, we would call:
         # >>> y = z.solve_for(y)
         # and our inputs in this function would be:
-        #    self = SymPint(name='z',expression=x*y)
-        #    other = SymPint(name='y',expression=y)
+        #    self = SymDim(name='z',expression=x*y)
+        #    other = SymDim(name='y',expression=y)
 
         # create an equation, e.g. sympy.Eq(z,x*y)
         eq = sympy.Eq(self.symbol,self.expression)
         # solve the equation for other, e.g. sympy.solve(z=x*y,y), which returns z/x
         expressions = sympy.solve(eq,other.expression)
-        # Create a new SymPint with name of what we are solving for equal to expression
-        outputs = [SymPint(name=other.name,expression=expression) for expression in expressions]
+        # Create a new SymDim with name of what we are solving for equal to expression
+        
+        outputs = [SymDim(name=other.name,expression=expression) for expression in expressions if sympy.I not in expression.atoms()]
         # Evaluate the expression to get units
         [output.evaluate() for output in outputs]
         return outputs
@@ -122,12 +124,12 @@ class SymPint:
         self.set_expression(sympy.simplify(self.expression))
         
     def evaluate(self):
-        # if our expression is a Symbol, set our SymPint's values to the Symbol's parent's values
+        # if our expression is a Symbol, set our SymDim's values to the Symbol's parent's values
         if isinstance(self.expression,Symbol):
             self.symbol = self.expression.parent.symbol
             self.valueKnown = self.expression.parent.valueKnown
             self.unit = self.expression.parent.unit
-        elif isinstance(self.expression,SymPint):
+        elif isinstance(self.expression,SymDim):
             self.valueKnown = self.expression.valueKnown
             self.unit = self.expression.unit
         elif isinstance(self.expression,sympy.Number):
@@ -136,7 +138,7 @@ class SymPint:
         else:
             if self.expression.is_Mul or self.expression.is_Add:
                 for i,arg_sympy in enumerate(self.expression.args):
-                    arg = SymPint(expression=arg_sympy)
+                    arg = SymDim(expression=arg_sympy)
                     arg.evaluate()                    
                     if i == 0:
                         output = arg
@@ -146,13 +148,13 @@ class SymPint:
                         elif self.expression.is_Add:
                             output = output + arg
             elif self.expression.is_Pow:
-                arg = SymPint(expression=self.expression.as_base_exp()[0])
-                exponent = SymPint(expression=self.expression.as_base_exp()[1])
+                arg = SymDim(expression=self.expression.as_base_exp()[0])
+                exponent = SymDim(expression=self.expression.as_base_exp()[1])
                 arg.evaluate()
                 exponent.evaluate()
                 output = arg**exponent
             else:
-                raise ValueError('operation {} not implemented'.format(operator))
+                raise ValueError('expression {} not implemented'.format(self.expression))
             self.valueKnown = output.valueKnown
             self.unit = output.unit
         
@@ -164,7 +166,7 @@ class SymPint:
         
     def derivative(self,diff):
         derivative = sympy.diff(self.expression,diff.symbol)
-        result = SymPint(expression=derivative)
+        result = SymDim(expression=derivative)
         result.evaluate()
         return result
         
@@ -172,28 +174,28 @@ class SymPint:
     def integrate(self,diff,lo=None,hi=None):
         if lo is None and hi is None:
             integrand = sympy.integrate(self.expression,diff.symbol)
-            result = SymPint(expression=integrand)
+            result = SymDim(expression=integrand)
             result.evaluate()
             return result
         elif lo is not None and hi is not None:
-            if not isinstance(lo,SymPint):
+            if not isinstance(lo,SymDim):
                 if lo == 0:
-                    lo = SymPint(0)
+                    lo = SymDim(0)
                 else:
                     if diff.unit.unit == u.dimensionless_unscaled:
                         name = diff.name+'_\\mathrm{lo}='+self.latex_float(lo)
                     else:
                         name = '\\left['+diff.name+'_\\mathrm{lo}='+self.latex_float(lo)+'\\,'+diff.unit.decompose().unit._repr_latex_().replace('$','')+'\\right]'
-                    lo = SymPint(name=name,unit=diff.unit.unit,value=lo)
-            if not isinstance(hi,SymPint):
+                    lo = SymDim(name=name,unit=diff.unit.unit,value=lo)
+            if not isinstance(hi,SymDim):
                 if hi == 0:
-                    hi = SymPint(0)
+                    hi = SymDim(0)
                 else:
                     if diff.unit.unit == u.dimensionless_unscaled:
                         name = diff.name+'_\\mathrm{hi}='+self.latex_float(hi)
                     else:
                         name = '\\left['+diff.name+'_\\mathrm{hi}='+self.latex_float(hi)+'\\,'+diff.unit.decompose().unit._repr_latex_().replace('$','')+'\\right]'
-                    hi = SymPint(name=name,unit=diff.unit.unit,value=hi)
+                    hi = SymDim(name=name,unit=diff.unit.unit,value=hi)
                 #hi = sympy.Number(hi)
             integrand_hi = self.integrate(diff)
             integrand_hi.substitute(diff,hi)
@@ -222,16 +224,18 @@ class SymPint:
     def __intdiv__ (self,other):
         return self.override_operator(other,'__intdiv__')
     def __pow__(self,other):
+        #pdb.set_trace()
         return self.override_operator(other,'__pow__')
     
     def override_operator(self,other,operator):
-        if not isinstance(other,SymPint):
+        #pdb.set_trace()
+        if not isinstance(other,SymDim):
             #try:
                 # other is an expression
-             #   other = SymPint(expression=other)
+             #   other = SymDim(expression=other)
             #except:
                 # other is an int or float
-                other = SymPint(other)
+                other = SymDim(other)
         try:
             expression = getattr(self.expression,operator)(other.expression)
         except:
@@ -244,8 +248,8 @@ class SymPint:
         else:
             unit = None
         valueKnown = self.valueKnown and other.valueKnown
-        # return unnamed SymPint
-        return SymPint(name='\mathrm{unnamed\,expr.}',unit=unit,expression=expression,valueKnown=valueKnown)
+        # return unnamed SymDim
+        return SymDim(name='\mathrm{unnamed\,expr.}',unit=unit,expression=expression,valueKnown=valueKnown)
 
     # this function is from Lauritz V. Thaulow's answer on 
     # https://stackoverflow.com/questions/13490292/format-number-using-latex-notation-in-python
@@ -258,7 +262,7 @@ class SymPint:
             return float_str
     
     def __repr__(self):
-        return 'SymPint(name={},expression={},unit={},valueKnown={})'.format(self.name,self.expression,self.unit,self.valueKnown)
+        return 'SymDim(name={},expression={},unit={},valueKnown={})'.format(self.name,self.expression,self.unit,self.valueKnown)
 
     def _repr_latex_(self):
         latex_rep = []
